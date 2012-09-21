2f7655dc50b0ee199b8afac02175b6ca013bf655
pbmolini <pbmolini@fbk.eu>
2012-09-21 17:27:09 +0200

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
 # Ensuring email uniqueness by downcasing the email attribute.
-	before_save { |user| user.email = email.downcase } 
+	before_save { self.email.downcase! } # before_save { |user| user.email = email.downcase } 
 
   validates :name , presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
