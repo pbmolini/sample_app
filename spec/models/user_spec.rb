9f58c07feee55f61d08d582401080eb9a228a307
pbmolini <pbmolini@fbk.eu>
2012-10-15 11:20:03 +0200

    problem corrected on a test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -157,8 +157,9 @@ describe "when email format is invalid" do
     end
 
     it "should destroy associated microposts" do
-      microposts = @user.microposts
+      microposts = @user.microposts.dup
       @user.destroy
+      microposts.should_not be_empty
       microposts.each do |micropost|
         Micropost.find_by_id(micropost.id).should be_nil
       end
